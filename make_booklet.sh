cde199c16f15e398c2765b924f8097c16ffaf3cb
Vladar4 <vladar4@gmail.com>
2019-04-12 01:23:28 +0300

    updated booklet script

--- make_booklet.sh
+++ make_booklet.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pdfjam itdr.pdf --letterpaper --keepinfo -o itdr_tmp.pdf
+pdfjam itdr_current.pdf --letterpaper --keepinfo -o itdr_tmp.pdf
 pdfjam --booklet true --landscape --keepinfo itdr_tmp.pdf -o itdr_booklet.pdf
 rm itdr_tmp.pdf
 exit 0
