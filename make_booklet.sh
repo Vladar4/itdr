7d0df67859bc1f01a1b66112fa61033c110b13e1
Vladar4 <vladar4@gmail.com>
2021-08-31 15:21:02 +0300

    booklet.sh: fix minor typos

--- make_booklet.sh
+++ make_booklet.sh
@@ -10,7 +10,7 @@ ROTATE=0            # rotate odd pages upside down
 # OPTIONS
 DELTA="-0.85in 0in" # space between pages (horizontal vertical)
 SCALE="0.72"        # absolute page scale
-OFFSET="-0in 0in"   # displace page origins (to_outer to_top)
+OFFSET="0in 0in"    # displace page origins (to_outer to_top)
 
 if [ $# -gt 0 ]
 then
@@ -36,7 +36,7 @@ else
     echo Usage: sh make_booklet.sh [-r] [FILE]...
     echo -r : rotate odd pages upside down
     echo
-    echo Recommended trim size: 5.5in x 8in
+    echo Recommended trim size: 5.4in x 8in
 fi
 
 exit 0
